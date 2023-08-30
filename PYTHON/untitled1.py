810fa47a248d83396c101840e9211cdc35057856
Omkar Shinde <131782542+omkar-shinde-21@users.noreply.github.com>
2023-08-30 14:35:45 +0530

    Add files via upload

---
+++ PYTHON/untitled1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 13:42:24 2023
+
+@author: Administrator
+"""
+
+list=['a','b','c','d','e']
+print(list,1)
+import mysql.connector
+
+db=mysql.connect(
+    host=local)
